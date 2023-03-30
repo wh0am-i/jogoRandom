d6f9131fdb6fdbc0c08f64a2a30e9d7b56b3dad6
Pedro <nonameeee6666@gmail.com>
2023-03-30 09:39:22 -0300

    30.03

---
+++ app/src/main/java/com/example/jogorandom/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.jogorandom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText entrada;
+    TextView saida;
+    Button confirmabtt, gerarbtt;
+    int numero = 0;
+    Random gerador = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        entrada = findViewById(R.id.entrada);
+        saida = findViewById(R.id.saida);
+        confirmabtt = findViewById(R.id.confirmabtt);
+        confirmabtt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirma();
+            }
+        });
+        gerarbtt = findViewById(R.id.gerarbtt);
+        gerarbtt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gerar();
+            }
+        });
+    }
+
+    public void confirma() {
+        int digitado = Integer.parseInt(entrada.getText().toString());
+        if (digitado > numero) {
+
+        } else if (digitado < numero) {
+
+        } else if (numero != 0 && digitado == numero) {
+
+        } else {
+            Toast.makeText(this, "", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void gerar() {
+        tentativas = 0;
+        numero = gerador.nextInt(100) + 1;//gera números de 0 à 99 -> o +1 faz não ter o 0 e ir até 100
+    }
+}
